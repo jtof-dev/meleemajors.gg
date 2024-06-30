f9b889860a567189cd6d3574562db573f3c4b201
JJTofflemire <jtof-dev@pm.me>
2024-06-30 14:57:06 -0700

    fix: correct image cleaner again
    Co-authored-by: Nathan <nathanbabcock@users.noreply.github.com>

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -187,16 +187,13 @@ async fn scrape_data(
     ); // ---> result
 
     // get larger image url
-    let image_cleaner_regex = Regex::new(r"^(.*?\.(jpg|png))").unwrap();
     let images = data_event["tournament"]["images"].as_array().unwrap();
     let largest_image = images
         .iter()
         .max_by_key(|img| img["width"].as_u64().unwrap())
         .unwrap();
     let largest_image_url = largest_image["url"].as_str().unwrap();
-    let cleaned_largest_image_url = image_cleaner_regex
-        .captures(largest_image_url)
-        .map_or("", |cap| cap.get(1).map_or("", |m| m.as_str())); // ---> result
+    let cleaned_largest_image_url = Regex::new(r"\?.*").unwrap().replace(largest_image_url, "");
 
     // get start.gg url
     let startgg_url = format!(
