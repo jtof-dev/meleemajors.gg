f3a235452bc64b082c6735b803340e54a4cc93e9
nbabcock <nathan.r.babcock@gmail.com>
2024-10-01 15:44:44 -0500

    feat: integrate with Kit (aka ConvertKit) API v3

--- ssg/src/mailing_list.rs
+++ ssg/src/mailing_list.rs
@@ -17,95 +17,91 @@ const DEV_GROUP_ID: &'static str = "e3Ow7r";
 
 pub struct MailingListService {
     client: Client,
-    campaigns: Vec<Value>,
+    api_secret: String,
+    broadcasts: Vec<Value>,
 }
 
 impl MailingListService {
     pub fn new() -> Self {
-        let api_token = get_sender_api_token();
+        let api_secret = get_kit_api_secret();
         let mut headers = HeaderMap::new();
-        headers.insert(
-            header::AUTHORIZATION,
-            HeaderValue::from_str(&format!("Bearer {}", api_token)).unwrap(),
-        );
-        headers.insert(
-            header::CONTENT_TYPE,
-            HeaderValue::from_static("application/json"),
-        );
         headers.insert(header::ACCEPT, HeaderValue::from_static("application/json"));
+
+        // Note: v4 API will use standard Auth/bearer token,
+        // but v3 uses a custom query param for api secret (??)
+
+        // headers.insert(
+        //     header::AUTHORIZATION,
+        //     HeaderValue::from_str(&format!("Bearer {}", api_secret)).unwrap(),
+        // );
+
         let client = Client::builder().default_headers(headers).build().unwrap();
-        let campaigns = Vec::new();
-        Self { client, campaigns }
-    }
-
-    /// https://api.sender.net/campaigns/get-all/
-    pub async fn get_all_campaigns(&mut self) -> reqwest::Result<&Vec<Value>> {
-        if self.campaigns.is_empty() {
-            let response = self
-                .client
-                .get("https://api.sender.net/v2/campaigns")
-                .query(&[("limit", "1000"), ("status", "scheduled")])
-                .send()
-                .await?
-                .error_for_status()?;
-            let json = response.json::<Value>().await?["data"]
-                .as_array()
-                .unwrap()
-                .to_vec();
-            self.campaigns = json;
+        let broadcasts = Vec::new();
+        Self {
+            client,
+            api_secret,
+            broadcasts,
         }
-        reqwest::Result::Ok(&self.campaigns)
-    }
-
-    async fn get_existing_campaign(&mut self, title: &str) -> Option<&Value> {
-        self.get_all_campaigns()
-            .await
-            .ok()?
-            .iter()
-            .find(|campaign| campaign["title"].as_str().unwrap() == title)
     }
 
     pub async fn schedule_tournament_emails(&mut self, tournament_data: &Value) {
-        // Check for existing campaign
+        // Check for existing broadcast
         let tournament_name = tournament_data["name"].as_str().unwrap();
-        let campaign_name = get_name(tournament_name);
-        let existing_campaign = self.get_existing_campaign(&campaign_name).await;
-        if existing_campaign.is_some() {
-            println!("{}", Green.paint("- Campaign already scheduled"));
+        let subject = get_subject_for_tournament(tournament_name);
+        let existing_broadcast = self.get_broadcast_by_subject(&subject).await;
+        if existing_broadcast.is_some() {
+            println!("{}", Green.paint("- Broadcast already scheduled"));
             return;
         }
 
-        // Create campaign
-        let campaign_id: String;
-        let res_create = self.create_campaign(&campaign_name, &tournament_data).await;
+        // Create broadcast
+        let broadcast_id: String;
+        let res_create = self.create_broadcast_v3(&subject, &tournament_data).await;
         match res_create {
             Ok(json) => {
-                println!("{}", Green.paint("- Created campaign"));
-                campaign_id = json["data"]["id"].as_str().unwrap().to_string();
+                println!("{}", Green.paint("- Created broadcast"));
+                broadcast_id = json["broadcast"]["id"].as_number().unwrap().to_string();
             }
             Err(err) => {
-                println!("{}", Red.paint("- Failed to create campaign"));
+                println!("{}", Red.paint("- Failed to create broadcast"));
                 println!("{:?}", err);
                 return;
             }
         }
 
-        sleep(Duration::from_millis(1000)).await;
-
-        // Schedule campaign
-        let res_schedule = self.schedule_campaign(&campaign_id, tournament_data).await;
-        match res_schedule {
-            Ok(_) => println!("{}", Green.paint("- Scheduled campaign")),
-            Err(_) => {
-                println!("{}", Red.paint("- Failed to schedule campaign"));
-                let errors = self.get_campaign_errors(&campaign_id).await.unwrap();
-                println!("{}", Red.paint(to_string_pretty(&errors).unwrap()));
-            }
-        }
+        // todo: update broadcast if needed
     }
 
-    /// https://api.sender.net/campaigns/create-campaign/
-    async fn create_campaign(&self, name: &str, tournament_data: &Value) -> Result<Value> {
+    /// - V3: https://developers.kit.com/v3#list-broadcasts
+    /// - V4: https://developers.kit.com/v4?shell#list-broadcasts
+    pub async fn list_broadcasts(&mut self) -> Result<&Vec<Value>> {
+        if self.broadcasts.is_empty() {
+            let response = self
+                .client
+                .get("https://api.convertkit.com/v3/broadcasts")
+                .query(&[("api_secret", &self.api_secret)])
+                .send()
+                .await?
+                .error_for_status()?;
+            let json = response.json::<Value>().await?["broadcasts"]
+                .as_array()
+                .unwrap()
+                .to_vec();
+            self.broadcasts = json;
+        }
+        Result::Ok(&self.broadcasts)
+    }
+
+    async fn get_broadcast_by_subject(&mut self, subject: &str) -> Option<&Value> {
+        self.list_broadcasts()
+            .await
+            .ok()?
+            .iter()
+            .find(|campaign| campaign["subject"].as_str().unwrap() == subject)
+    }
+
+    /// https://developers.kit.com/v3#list-broadcasts
+    async fn create_broadcast_v3(&self, subject: &str, tournament_data: &Value) -> Result<Value> {
         let content_template = r#"
             <b>{{name}}</b> is this weekend, {{date}}.<br>
             feat. {{player0}}, {{player1}}, {{player2}}, {{player3}}, {{player4}}, {{player5}}, {{player6}}, {{player7}}, and more.<br>
@@ -113,89 +109,89 @@ impl MailingListService {
         "#;
         let content = replace_placeholder_values(tournament_data, content_template);
         let html = read_file("html/email.html")
-            .replace("{{name}}", &name)
+            .replace("{{name}}", &subject)
             .replace("{{content}}", &content);
-        let response = self
+        let req = self
             .client
-            .post("https://api.sender.net/v2/campaigns")
+            .post("https://api.convertkit.com/v3/broadcasts")
             .json(&json!({
-                "title": name,
-                "preheader": "This weekend...", // todo
-                "subject": name,
-                "from": FROM_NAME,
-                "reply_to": FROM_EMAIL,
-                "content_type": "html",
-                "groups": [DEV_GROUP_ID],
-                "content": html,
-            }))
-            .send()
-            .await?
-            .error_for_status()?;
-        let json = response.json::<Value>().await?;
-        Ok(json)
-    }
-
-    /// https://api.sender.net/campaigns/schedule-send/
-    async fn schedule_campaign(&self, campaign_id: &str, tournament_data: &Value) -> Result<Value> {
-        let url = format!("https://api.sender.net/v2/campaigns/{campaign_id}/schedule");
-        // schedule time is always in Sender.net account timezone:
-        // GMT-06:00 Central Time
-        // Y-m-d H:i:s
-        let request = self.client.post(&url).json(&json!({
-            "schedule_time": "2024-10-02 05:40:00"
-        }));
-        let response = request.send().await?;
-        let status = response.status();
-        let json = response.json::<Value>().await?;
+                "api_secret": &self.api_secret,
+                "email_layout_template": Value::Null, // todo
+                "content": content,
+            }));
+        let res = req.send().await?;
+        let status = res.status();
+        let json = res.json::<Value>().await?;
         if status.is_success() {
             Ok(json)
         } else {
-            eprintln!(
-                "{}",
-                Red.paint(format!("Response code {}", status.as_str()))
-            );
+            let response_code_str = format!("Response code {}", status.as_str());
+            eprintln!("{}", Red.paint(&response_code_str));
             eprintln!("{}", Red.paint(to_string_pretty(&json).unwrap()));
-            Err(anyhow!("Response code: {}", status).context(json))
+            Err(anyhow!(response_code_str).context(json))
         }
     }
 
-    /// https://api.sender.net/campaigns/errors/
-    async fn get_campaign_errors(&self, campaign_id: &str) -> Result<Value> {
-        let url = format!("https://api.sender.net/v2/campaigns/{campaign_id}/errors");
-        let request = self.client.get(&url);
-        let response = request.send().await?;
-        let status = response.status();
-        let json = response.json::<Value>().await?;
+    /// https://developers.kit.com/v4?shell#create-a-broadcast
+    async fn create_broadcast_v4(&self, subject: &str, tournament_data: &Value) -> Result<Value> {
+        let content_template = r#"
+            <b>{{name}}</b> is this weekend, {{date}}.<br>
+            feat. {{player0}}, {{player1}}, {{player2}}, {{player3}}, {{player4}}, {{player5}}, {{player6}}, {{player7}}, and more.<br>
+            <a href="{{start.gg-url}}" target="blank">View bracket on Start.gg</a><br>
+        "#;
+        let content = replace_placeholder_values(tournament_data, content_template);
+        let html = read_file("html/email.html")
+            .replace("{{name}}", &subject)
+            .replace("{{content}}", &content);
+        let req = self
+            .client
+            .post("https://api.kit.com/v4/broadcasts")
+            .json(&json!({
+                "email_template_id": Value::Null, // todo: create template
+                "broadcast_id": Value::Null,
+                "content": content,
+                "description": Value::Null,
+                "public": false, // false == draft
+                "published_at": Value::Null,
+                "send_at": Value::Null, // The scheduled send time for this broadcast in ISO8601 format
+                "thumbnail_alt": Value::Null,
+                "preview_text": "This weekend...", // todo
+                "subject": subject,
+                "subscriber_filter": Value::Null, // todo: dev vs all
+            }));
+        let res = req.send().await?;
+        let status = res.status();
+        let json = res.json::<Value>().await?;
         if status.is_success() {
             Ok(json)
         } else {
-            eprintln!(
-                "{}",
-                Red.paint(format!("Response code {}", status.as_str()))
-            );
+            let response_code_str = format!("Response code {}", status.as_str());
+            eprintln!("{}", Red.paint(&response_code_str));
             eprintln!("{}", Red.paint(to_string_pretty(&json).unwrap()));
-            Err(anyhow!("Response code: {}", status).context(json))
+            Err(anyhow!(response_code_str).context(json))
         }
     }
 }
 
-fn get_sender_api_token() -> String {
-    if let Ok(api_token) = std::env::var("SENDER_API_TOKEN") {
+fn get_kit_api_secret() -> String {
+    let env_key = "KIT_V3_API_SECRET";
+    if let Ok(api_token) = std::env::var(env_key) {
         return api_token;
     }
 
-    let api_url = "https://app.sender.net/settings/tokens";
-    eprintln!("{}", Red.paint("Missing API token for Sender"));
+    let api_url = "https://app.kit.com/account_settings/developer_settings";
+    eprintln!("{}", Red.paint("Missing API secret for Kit"));
     println!("{}", Yellow.paint("Generate one here:"));
     println!("{}", Yellow.paint(api_url));
     println!("{}", Yellow.paint("Then add it to .env or run.sh"));
-    panic!("SENDER_API_TOKEN must be set");
+    println!("{}", Yellow.paint(format!("{}=your-api-secret", env_key)));
+    panic!("Missing API Secret env var");
 }
 
-fn get_name(tournament_name: &str) -> String {
+fn get_subject_for_tournament(tournament_name: &str) -> String {
     format!("Tournament reminder: {}", tournament_name)
 }
 
-fn get_name_top8(tournament_name: &str) -> String {
+fn get_subject_for_top8(tournament_name: &str) -> String {
     format!("Top 8 starting soon: {}", tournament_name)
 }
